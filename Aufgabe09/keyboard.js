073661df36df07bf91e12577899b9784e649a4ba
Schwer5 <piaschwer@gmail.com>
2022-12-09 15:34:01 +0100

    aufnahmebutton

--- Aufgabe09/keyboard.js
+++ Aufgabe09/keyboard.js
@@ -1,50 +1,86 @@
 const button1 = document.querySelector('.button1');
 button1.addEventListener('click', function () {
     playsample(new Audio('a.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('a.mp3');
+    }
 });
 const button2 = document.querySelector('.button2');
 button2.addEventListener('click', function () {
     playsample(new Audio('af.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('af.mp3');
+    }
 });
 const button3 = document.querySelector('.button3');
 button3.addEventListener('click', function () {
     playsample(new Audio('b.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('b.mp3');
+    }
 });
 const button4 = document.querySelector('.button4');
 button4.addEventListener('click', function () {
     playsample(new Audio('bf.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('bf.mp3');
+    }
 });
 const button5 = document.querySelector('.button5');
 button5.addEventListener('click', function () {
     playsample(new Audio('c.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('c.mp3');
+    }
 });
 const button6 = document.querySelector('.button6');
 button6.addEventListener('click', function () {
     playsample(new Audio('d.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('d.mp3');
+    }
 });
 const button7 = document.querySelector('.button7');
 button7.addEventListener('click', function () {
     playsample(new Audio('df.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('df.mp3');
+    }
 });
 const button8 = document.querySelector('.button8');
 button8.addEventListener('click', function () {
     playsample(new Audio('e.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('e.mp3');
+    }
 });
 const button9 = document.querySelector('.button9');
 button9.addEventListener('click', function () {
     playsample(new Audio('ef.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('ef.mp3');
+    }
 });
 const button10 = document.querySelector('.button10');
 button10.addEventListener('click', function () {
     playsample(new Audio('f.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('f.mp3');
+    }
 });
 const button11 = document.querySelector('.button11');
 button11.addEventListener('click', function () {
     playsample(new Audio('g.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('g.mp3');
+    }
 });
 const button12 = document.querySelector('.button12');
 button12.addEventListener('click', function () {
     playsample(new Audio('gf.mp3'));
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('gf.mp3');
+    }
 });
 const playbutton = document.querySelector('.playbutton');
 let intervalID = 0;
@@ -79,6 +115,17 @@ shuffle.addEventListener('click', function () {
         let temporary = array[j];
         array[j] = array[i];
         array[i] = temporary;
+    }
+});
+const micro = document.querySelector('.microphonebutton');
+micro.addEventListener('click', function () {
+    if (micro.classList.contains('active')) {
+        micro.classList.remove('active');
+    }
+    else {
+        micro.classList.add('active');
+        clearInterval(intervalID);
+        array = [];
     }
 });
 //# sourceMappingURL=keyboard.js.map
--- Aufgabe09/keyboard.ts
+++ Aufgabe09/keyboard.ts
@@ -1,61 +1,98 @@
 const button1 = document.querySelector('.button1') as HTMLButtonElement
 button1.addEventListener('click', function () {
     playsample(new Audio('a.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('a.mp3')
+    }
+
 })
 
 const button2 = document.querySelector('.button2') as HTMLButtonElement
 button2.addEventListener('click', function () {
     playsample(new Audio('af.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('af.mp3')
+    }
 })
 
 const button3 = document.querySelector('.button3') as HTMLButtonElement
 button3.addEventListener('click', function () {
     playsample(new Audio('b.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('b.mp3')
+    }
 })
 
 const button4 = document.querySelector('.button4') as HTMLButtonElement
 button4.addEventListener('click', function () {
     playsample(new Audio('bf.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('bf.mp3')
+    }
 })
 
 const button5 = document.querySelector('.button5') as HTMLButtonElement
 button5.addEventListener('click', function () {
     playsample(new Audio('c.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('c.mp3')
+    }
 })
 
 const button6 = document.querySelector('.button6') as HTMLButtonElement
 button6.addEventListener('click', function () {
     playsample(new Audio('d.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('d.mp3')
+    }
 })
 
 const button7 = document.querySelector('.button7') as HTMLButtonElement
 button7.addEventListener('click', function () {
     playsample(new Audio('df.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('df.mp3')
+    }
 })
 
 const button8 = document.querySelector('.button8') as HTMLButtonElement
 button8.addEventListener('click', function () {
     playsample(new Audio('e.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('e.mp3')
+    }
 })
 
 const button9 = document.querySelector('.button9') as HTMLButtonElement
 button9.addEventListener('click', function () {
     playsample(new Audio('ef.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('ef.mp3')
+    }
 })
 
 const button10 = document.querySelector('.button10') as HTMLButtonElement
 button10.addEventListener('click', function () {
     playsample(new Audio('f.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('f.mp3')
+    }
 })
 
 const button11 = document.querySelector('.button11') as HTMLButtonElement
 button11.addEventListener('click', function () {
     playsample(new Audio('g.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('g.mp3')
+    }
 })
 
 const button12 = document.querySelector('.button12') as HTMLButtonElement
 button12.addEventListener('click', function () {
     playsample(new Audio('gf.mp3'))
+    if (document.querySelector('.microphonebutton').classList.contains('active')) {
+        array.push('gf.mp3')
+    }
 })
 
 const playbutton = document.querySelector('.playbutton') as HTMLImageElement
@@ -95,5 +132,19 @@ shuffle.addEventListener('click', function () {
         let temporary = array[j]
         array[j] = array[i]
         array[i] = temporary
+    }
+})
+
+const micro = document.querySelector('.microphonebutton') as HTMLImageElement
+micro.addEventListener('click', function () {
+
+    if (micro.classList.contains('active')) {
+        micro.classList.remove('active')
+
+    }
+    else {
+        micro.classList.add('active')
+        clearInterval(intervalID)
+        array = []
     }
 })
