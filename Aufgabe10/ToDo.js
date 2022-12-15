a49743e8b3a00748fb875e5003166077219bc6b9
Schwer5 <piaschwer@gmail.com>
2022-12-15 21:53:04 +0100

    äjkb

--- Aufgabe10/ToDo.js
+++ Aufgabe10/ToDo.js
@@ -1,40 +1,45 @@
-var todo = [{
+var todo = [
+    {
         name: 'Putzen'
     },
     {
         name: 'Essen einkaufen'
     },
-    { name: 'Freudentanz' }
+    {
+        name: 'Freudentanz'
+    }
 ];
 function h2() {
     var ueberschrift = document.querySelector('.anzahl');
-    ueberschrift.innerHTML = todo.length + ' in total';
+    var anzahl = document.querySelectorAll('li').length;
+    ueberschrift.innerHTML = anzahl + ' in total';
 }
 function tabelle() {
     for (var index = 0; index < todo.length; index++) {
-        tabelleadd(todo[index].name);
+        tabelleadd(todo[index]);
     }
 }
 function eingabe(eintippen) {
     var newtodo = { name: eintippen };
     todo.push(newtodo);
-    tabelleadd(newtodo.name);
+    tabelleadd(newtodo);
 }
-function tabelleadd(addstring) {
+function tabelleadd(addtodo) {
     var table = document.querySelector('.todo');
     var machen = document.createElement("li");
-    machen.textContent = addstring;
     var checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     var muell = document.createElement('i');
-    muell.addEventListener('click', function () { muell.parentElement.remove(); });
+    muell.addEventListener('click', function () { muell.parentElement.remove(); h2(); });
     muell.classList.add('fa-trash');
+    muell.classList.add('fa-5x');
     muell.classList.add('fas');
     machen.appendChild(checkbox);
+    machen.append(addtodo.name);
     machen.appendChild(muell);
     table.appendChild(machen);
+    h2();
 }
-h2();
 tabelle();
 var textinput = document.getElementById('neuetodoeinfuegen');
 var form = document.querySelector('.inputform');
--- Aufgabe10/ToDo.ts
+++ Aufgabe10/ToDo.ts
@@ -2,13 +2,16 @@ interface ToDo {
     name: string;
 }
 
-let todo: ToDo[] = [{
-    name: 'Putzen'
-},
-{
-    name: 'Essen einkaufen'
-},
-{ name: 'Freudentanz' }
+let todo: ToDo[] = [
+    {
+        name: 'Putzen'
+    },
+    {
+        name: 'Essen einkaufen'
+    },
+    {
+        name: 'Freudentanz'
+    }
 ]
 
 
@@ -16,42 +19,43 @@ let todo: ToDo[] = [{
 
 function h2() {
     const ueberschrift = document.querySelector('.anzahl') as HTMLHeadingElement
-    ueberschrift.innerHTML = todo.length + ' in total'
+    let anzahl = document.querySelectorAll('li').length
+    ueberschrift.innerHTML = anzahl + ' in total'
 }
 
 
 function tabelle() {
-    
     for (let index: number = 0; index < todo.length; index++) {
-        tabelleadd(todo[index].name)
+        tabelleadd(todo[index])
     }
 }
 
 function eingabe(eintippen: string) {
     let newtodo: ToDo = { name: eintippen }
     todo.push(newtodo)
-    tabelleadd (newtodo.name)
+    tabelleadd(newtodo)
 }
 
-function tabelleadd(addstring: string){
+function tabelleadd(addtodo: ToDo) {
     const table = document.querySelector('.todo') as HTMLHeadingElement
     const machen = document.createElement("li")
-    machen.textContent = addstring;
+    
     let checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
     let muell = document.createElement('i')
-    muell.addEventListener('click', function(){muell.parentElement.remove()})
+    muell.addEventListener('click', function () { muell.parentElement.remove(); h2() })
     muell.classList.add('fa-trash')
+    muell.classList.add('fa-5x')
     muell.classList.add('fas')
     machen.appendChild(checkbox)
+    machen.append( addtodo.name)
     machen.appendChild(muell)
     table.appendChild(machen)
-
+    h2()
 }
 
-h2()
 tabelle()
 
 const textinput = document.getElementById('neuetodoeinfuegen') as HTMLInputElement
 const form = document.querySelector('.inputform') as HTMLFormElement
-form.addEventListener('submit', function () {eingabe(textinput.value) })
+form.addEventListener('submit', function () { eingabe(textinput.value) })
